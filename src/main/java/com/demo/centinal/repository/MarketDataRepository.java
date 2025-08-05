7411afd31bd05d72cb0d02b721b39232c8440fc6
seanrogan <srogan88@gmail.com>
2025-08-05 00:21:38 -0400

    fixing import

--- src/main/java/com/demo/centinal/repository/MarketDataRepository.java
+++ src/main/java/com/demo/centinal/repository/MarketDataRepository.java
@@ -3,6 +3,7 @@ package com.demo.centinal.repository;
 import com.demo.centinal.entities.MarketData;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.security.authentication.jaas.JaasPasswordCallbackHandler;
 import org.springframework.stereotype.Repository;
 
